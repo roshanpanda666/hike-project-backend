60a9d5cc1cd95632204541345e2f297c9914d11e
roshan <roshanpanda666@gmail.com>
2026-08-08 20:50:44 +0530

    user just signed up

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,5 +1,5 @@
 const User=require('../model/user')
-
+const jwt=require('jsonwebtoken')
 exports.signup=async (req,res,next)=>{
     try {
         const newUser=await User.create({
@@ -8,8 +8,10 @@ exports.signup=async (req,res,next)=>{
             password:req.body.password,
             number:req.body.number,
         })
+
+        const token=jwt.sign({id:newUser._id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_EXPIRES_IN})
         
-        res.status(201).json({status:"success",data:{user:newUser}});
+        res.status(201).json({status:"success",token,data:{user:newUser}});
     } catch (error) {
         console.error("Error creating user:", error);
         // 4. Send error response so the client isn't left hanging
